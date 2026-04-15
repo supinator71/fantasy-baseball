6b31b45b6af600f15727fa50e78eae43e814bf1f
supinator71 <supinator71@users.noreply.github.com>
2026-04-15 15:14:28 -0700

    Safely remove root diagnostic scripts

--- diag_yahoo.js
+++
@@ -1,8 +0,0 @@
-const yahooService = require('./server/services/yahooService');
-
-async function testSettings() {
-  try {
-    // Check if we can fetch settings for a mocked/fake league key, but we need an authenticated user.
-    // I can read the source code in `server/routes/yahoo.js` instead.
-  } catch(e) {}
-}
