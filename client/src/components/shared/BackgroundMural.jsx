476383cf6a35a22b1a3915063dcfcbd0c506c847
supinator71 <supinator71@users.noreply.github.com>
2026-04-15 13:11:48 -0700

    Adjust background mural card layout to peek out beyond internal dashboard boxes and sidebar bounds

--- client/src/components/shared/BackgroundMural.jsx
+++ client/src/components/shared/BackgroundMural.jsx
@@ -12,17 +12,17 @@ export default function BackgroundMural() {
       zIndex: -1,
       overflow: 'hidden'
     }}>
-      {/* Left Column Scatter */}
-      <img src="/card-hitter.png" alt="" style={{ position: 'absolute', top: '10%', left: '-5%', width: 320, opacity: 0.25, transform: 'rotate(-15deg)' }} />
-      <img src="/card-slide.png" alt="" style={{ position: 'absolute', top: '55%', left: '-8%', width: 380, opacity: 0.3, transform: 'rotate(25deg)' }} />
+      {/* Left Column Scatter (pushed right to avoid Sidebar) */}
+      <img src="/card-hitter.png" alt="" style={{ position: 'absolute', top: '10%', left: '15%', width: 320, opacity: 0.25, transform: 'rotate(-15deg)' }} />
+      <img src="/card-slide.png" alt="" style={{ position: 'absolute', top: '55%', left: '12%', width: 380, opacity: 0.3, transform: 'rotate(25deg)' }} />
       
       {/* Right Column Scatter */}
       <img src="/card-pitcher.png" alt="" style={{ position: 'absolute', top: '5%', right: '-5%', width: 350, opacity: 0.3, transform: 'rotate(15deg)' }} />
       <img src="/card-swing.png" alt="" style={{ position: 'absolute', top: '45%', right: '-10%', width: 380, opacity: 0.25, transform: 'rotate(-20deg)' }} />
 
-      {/* Bottom Scatter */}
-      <img src="/card-hitter.png" alt="" style={{ position: 'absolute', bottom: '-15%', right: '25%', width: 320, opacity: 0.15, transform: 'rotate(70deg)' }} />
-      <img src="/card-slide.png" alt="" style={{ position: 'absolute', bottom: '-20%', left: '20%', width: 380, opacity: 0.2, transform: 'rotate(-45deg)' }} />
+      {/* Bottom Scatter (raised up so they don't clip off the bottom of the monitor) */}
+      <img src="/card-hitter.png" alt="" style={{ position: 'absolute', bottom: '-2%', right: '25%', width: 360, opacity: 0.15, transform: 'rotate(70deg)' }} />
+      <img src="/card-slide.png" alt="" style={{ position: 'absolute', bottom: '-5%', left: '30%', width: 390, opacity: 0.2, transform: 'rotate(-45deg)' }} />
     </div>
   );
 }
