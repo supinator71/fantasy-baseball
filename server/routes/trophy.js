48b52086d9630f33d2cfd7e126923e10a20ae9a7
supinator71 <supinator71@users.noreply.github.com>
2026-04-17 15:57:16 -0700

    feat(gamification): generate and integrate new cyborg baseball cards expanding collection to 25 items

--- server/routes/trophy.js
+++ server/routes/trophy.js
@@ -4,22 +4,39 @@ const db = require('../services/database');
 
 // Master dictionary of all collectible cards in the application
 const CARD_COLLECTION = [
-  { id: 't1_hitter', name: 'Tier 1 Hitter', img: '/cyborg_card_tier1_hitter.png', rarity: 'common' },
-  { id: 't1_pitcher', name: 'Tier 1 Pitcher', img: '/cyborg_card_tier1_pitcher.png', rarity: 'common' },
-  { id: 't2_holo', name: 'Holographic Foil Base', img: '/cyborg_card_tier2_holo.png', rarity: 'uncommon' },
-  { id: 't3_prism', name: 'Diamond Prism Showcase', img: '/cyborg_card_tier3_prism.png', rarity: 'rare' },
-  
+  // Core Set (Base)
+  { id: 'base_hitter', name: 'Cyber Hitter (Base)', img: '/cyborg_card_tier1_hitter.png', rarity: 'common' },
+  { id: 'base_pitcher', name: 'Cyber Pitcher (Base)', img: '/cyborg_card_tier1_pitcher.png', rarity: 'common' },
+  { id: 'base_closer', name: 'Bullpen Closer (Base)', img: '/cyborg_bullpen_closer.png', rarity: 'common' },
+  { id: 'base_manager', name: 'Tactical Manager (Base)', img: '/cyborg_cyber_manager.png', rarity: 'common' },
+  { id: 'base_steal', name: 'Stealing Second (Base)', img: '/cyborg_stealing_second.png', rarity: 'common' },
+  { id: 'base_catch', name: 'Diving Catch (Base)', img: '/cyborg_diving_catch.png', rarity: 'common' },
   { id: 'gatorade', name: 'Gatorade Glitch', img: '/cyborg_gatorade_glitch.png', rarity: 'common' },
-  { id: 'steal', name: 'Stealing Second', img: '/cyborg_stealing_second.png', rarity: 'common' },
+
+  // Uncommon Variations (Foil / Minor visual tweaks)
+  { id: 't2_holo', name: 'Holographic Foil Base', img: '/cyborg_card_tier2_holo.png', rarity: 'uncommon' },
+  { id: 'coach_woman', name: 'Holographic Coach', img: '/cyborg_coach_card_woman.png', rarity: 'uncommon' },
+  { id: 'unc_closer', name: 'Bullpen Closer (Refractor)', img: '/cyborg_bullpen_closer.png', rarity: 'uncommon' },
+  { id: 'unc_manager', name: 'Tactical Manager (Foil)', img: '/cyborg_cyber_manager.png', rarity: 'uncommon' },
+  { id: 'unc_steal', name: 'Stealing Second (Hyper)', img: '/cyborg_stealing_second.png', rarity: 'uncommon' },
+  { id: 'unc_catch', name: 'Diving Catch (Glow Edition)', img: '/cyborg_diving_catch.png', rarity: 'uncommon' },
   
+  // Rare Variations (Prizms / Special Moments)
+  { id: 't3_prism', name: 'Diamond Prism Showcase', img: '/cyborg_card_tier3_prism.png', rarity: 'rare' },
   { id: 'sp_wide', name: 'Rookie Silver Prizm (Wide)', img: '/cyborg_silver_prism_wide.png', rarity: 'rare' },
   { id: 'sp_medium', name: 'Rookie Silver Prizm (Wall Rob)', img: '/cyborg_silver_prism_medium.png', rarity: 'rare' },
-  { id: 'sp_closeup', name: 'Rookie Silver Prizm (Visor)', img: '/cyborg_silver_prism_closeup.png', rarity: 'legendary' },
+  { id: 'jump_kid', name: 'Team Celebration Foil', img: '/cyborg_team_jump_kid.png', rarity: 'rare' },
+  { id: 'rare_walkoff', name: 'Walk-Off Homer (Silver Prizm)', img: '/cyborg_walkoff_homer.png', rarity: 'rare' },
+
+  // Epic Variations (Extremely hard to pull)
   { id: 'sp_hand', name: 'Rookie Silver Prizm (Pack Pull)', img: '/cyborg_silver_prism_hand.png', rarity: 'epic' },
-  
-  { id: 'arcana_hand', name: 'Homerun Arcana', img: '/cyborg_card_hand_arcana.png', rarity: 'legendary' },
-  { id: 'coach_woman', name: 'Holographic Coach', img: '/cyborg_coach_card_woman.png', rarity: 'uncommon' },
-  { id: 'jump_kid', name: 'Team Celebration Foil', img: '/cyborg_team_jump_kid.png', rarity: 'rare' }
+  { id: 'epic_catch', name: 'Diving Catch (Gold /10)', img: '/cyborg_diving_catch.png', rarity: 'epic' },
+  { id: 'epic_closer', name: 'Closer (Ruby Wave)', img: '/cyborg_bullpen_closer.png', rarity: 'epic' },
+
+  // Legendary (Signatures & Ultimate Pulls)
+  { id: 'sp_closeup', name: 'Rookie True Gold (Visor Edition)', img: '/cyborg_silver_prism_closeup.png', rarity: 'legendary' },
+  { id: 'arcana_hand', name: 'Homerun Arcana (Signed)', img: '/cyborg_card_hand_arcana.png', rarity: 'legendary' },
+  { id: 'leg_walkoff', name: 'Walk-Off Homer (Autograph Edition)', img: '/cyborg_walkoff_homer.png', rarity: 'legendary' }
 ];
 
 // Helper to pull a random card based on rarity weights
